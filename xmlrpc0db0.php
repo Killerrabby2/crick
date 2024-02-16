c9dd0350ca68a25a6a3a5fba32302accabf7196e
missnorwyu32 <144135159+missnorwyu32@users.noreply.github.com>
2024-02-16 17:00:48 +0600

    update

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://stage.helpmcafee.uk.com</homePageLink>
+		<homePageLink>https://uat.identity.engagementhub.pwc.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://stage.helpmcafee.uk.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://stage.helpmcafee.uk.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://stage.helpmcafee.uk.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://stage.helpmcafee.uk.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://stage.helpmcafee.uk.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://uat.identity.engagementhub.pwc.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://uat.identity.engagementhub.pwc.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://uat.identity.engagementhub.pwc.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://uat.identity.engagementhub.pwc.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://uat.identity.engagementhub.pwc.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
